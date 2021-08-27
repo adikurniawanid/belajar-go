4a714f1368a70cf62527a974d7bdce0b7314b88e
Adi Kurniawan <adikurniawan.dev@gmail.com>
2021-08-27 20:30:57 +0700

    Create 01-tipedata.go

---
+++ 01-tipedata.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Satu = ", 1)
+	fmt.Println("Dua = ", 2)
+	fmt.Println("Tiga Koma Lima = ", 3.5)
+
+	fmt.Println("Benar = ", true)
+	fmt.Println("Salah = ", false)
+
+	fmt.Println("Adi Kurniawan")
+	fmt.Println(len("Adi Kurniawan"))
+	fmt.Println("Adi Kurniawan"[1]) //ambil huruf dalam bentuk byte pada index ..
+}
