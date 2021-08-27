02686db656af27d68696df485cb0d75d207d9421
Adi Kurniawan <adikurniawan.dev@gmail.com>
2021-08-27 20:31:18 +0700

    Create 03-constant.go

---
+++ 03-constant.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// constant itu = final di java
+	const phi = 22 / 7
+
+	const (
+		firstName = "Adi"
+		lastName  = "Kurniawan"
+	)
+
+	fmt.Println(phi)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+
+}
