fff5423052a87af96db29a939471075c0e33a25d
Adi Kurniawan <adikurniawan.dev@gmail.com>
2021-08-28 08:32:30 +0700

    Create 09-tipeDataArray.go

---
+++ 09-tipeDataArray.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nama [2]string
+	nama[0] = "Adi"
+	nama[1] = "Kurniawan"
+
+	fmt.Println(nama[0])
+	fmt.Println(nama[1])
+
+	var values = [3]int{
+		90, 80,
+	}
+
+	// panjang array bukan banyak data
+	fmt.Println(len(values), len(nama))
+
+	fmt.Println(values)
+
+	var nilai = []int{
+		2, 3, 4, 5,
+	}
+	fmt.Println(len(nilai))
+
+	fmt.Println(nilai)
+
+}
