34ebbe1b64dd81908e8252dee86e22f738cd130f
Adi Kurniawan <adikurniawan.dev@gmail.com>
2021-08-28 08:21:25 +0700

    Create 07-operasiPerbandingan.go

---
+++ 07-operasiPerbandingan.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 10
+	var b = 20
+	var c = "Adi"
+	var d = "Adi"
+
+	fmt.Println(a > b)
+	fmt.Println(a < b)
+	fmt.Println(a == b)
+	fmt.Println(c == d)
+	fmt.Println(c != d)
+}
